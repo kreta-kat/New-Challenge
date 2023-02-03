5f4297ce3ec445f1f1a78215e1d36253bb797ec5
kreta-kat <85033106+kreta-kat@users.noreply.github.com>
2023-02-03 13:31:46 +0530

    Create Basic Recursion-5.cpp

---
+++ Recursion/Basic Recursion-5.cpp
@@ -0,0 +1,19 @@
+//print linearly from n to 1 BACKTRACKING
+#include<iostream>
+using namespace std;
+
+void func(int i, int n)
+{
+    if(i>n)
+    return;
+    func(i+1,n);
+    cout<<i<<endl;
+}
+int main(void)
+{
+    int n;
+    cout<<"ENter n"<<endl;
+    cin>>n;
+    func(1,n);
+    return 0; 
+}
