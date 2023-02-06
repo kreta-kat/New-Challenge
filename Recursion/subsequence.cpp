89d9c89f2a8b69c7fb010da940c77467bb043fa3
kreta-kat <85033106+kreta-kat@users.noreply.github.com>
2023-02-06 23:53:50 +0530

    Update subsequence.cpp

--- Recursion/subsequence.cpp
+++ Recursion/subsequence.cpp
@@ -8,14 +8,24 @@ void printF(int ind, vector<int> &ds, int arr[], int n)
     {
         cout<<it<<" ";
     }
-    //case for not pick
+    //case for not pick at all i.e. empty array
   if(ds.size() ==0)
   {
-     cout<<"{}"<<endl;
+     cout<<"{}";
 
   }
     cout<<endl;
     return;
+    }
+ 
+ //case for picking the element in the subsequence ds
+ ds.push_back(arr[ind]);
+ printF(ind+1,ds,arr,n);
+ //removing the last element to make not pick condition
+ ds.pop_back();
+ //case for not picking the element at that index moving to next elements
+ printF(ind+1,ds,arr,n); 
+
 }
 int main()
 {
