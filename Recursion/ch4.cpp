dfb68276ea1952a292b4d4b5653a1df9ae196037
kreta-kat <85033106+kreta-kat@users.noreply.github.com>
2023-02-07 21:06:00 +0530

    Create ch4.cpp

---
+++ Recursion/ch4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   /* int a =5;
+   a = (1,2,3); //brckett m assign R -> L
+   // a = 1,2,3; //NO brackett assign L -> R
+   cout<<a;
+   */
+
+    /* int x =2;
+   (x & 1)? cout<<"True": cout<<"False";  
+   */
+
+  /*
+    int i=10;
+    i++;
+    i*i;
+    cout<<i;
+  */
+   return 0;
+
+} 
