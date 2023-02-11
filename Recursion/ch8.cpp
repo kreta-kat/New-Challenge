470d5667bbb727fa05173b176c56fe6b33ba30f0
kreta-kat <85033106+kreta-kat@users.noreply.github.com>
2023-02-11 20:48:46 +0530

    Create ch8.cpp

---
+++ Recursion/ch8.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A,B,C;
+    cin>>A>>B>>C;
+    if(A>B && A>C)
+    {
+        if(B>C)
+        cout<<B<<endl;
+        else
+        cout<<C<<endl;
+    }
+    else if(B>A && B>C)
+    {
+        if(A>C)
+        cout<<A<<endl;
+        else
+        cout<<C<<endl;
+    }
+    else  //C
+    {
+        if(A>B)
+        cout<<A<<endl;
+        else
+        cout<<B<<endl;
+    }
+    return 0;
+}
