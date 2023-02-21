8db4858c93e911f1a62591fc973bd2c96e252cc2
kreta-kat <85033106+kreta-kat@users.noreply.github.com>
2023-02-21 23:00:00 +0530

    Create ch19.cpp

---
+++ Recursion/ch19.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+     
+    if(n == 0)
+    {   
+        return 1;
+    }
+
+    else if(n>0)
+    {
+       return n*fact(n-1);
+
+    }
+    else
+    {
+       return -1;
+    }
+    
+}
+int main()
+{
+    int n=5,r=2,i=0;
+    
+        cout<<fact(n)/(fact(n-r)*fact(r));
+
+    return 0;
+}
