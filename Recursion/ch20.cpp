daef9dc1b148d63f4b9255ca31bf197228ad1dd4
kreta-kat <85033106+kreta-kat@users.noreply.github.com>
2023-02-27 21:24:13 +0530

    Update ch20.cpp

--- Recursion/ch20.cpp
+++ Recursion/ch20.cpp
@@ -1,1 +1,20 @@
 //ode for
+#include <bits/stdc++.h> 
+#include <string>
+using namespace std;
+
+int main()
+{
+    vector<int> k = {1,2,3,4};
+    cout<<"Vector Size before insert "<<k.size()<<endl;
+    k.push_back(6);
+    k.push_back(7);
+    cout<<"Vector Size after insert "<<k.size()<<endl;
+    k.pop_back();
+    cout<<"Vector Size after remove "<<k.size()<<endl;
+    k.pop_back();
+    k.pop_back();
+    cout<<"Vector Size after remove -2 "<<k.size()<<endl;
+    return 0;
+
+}
