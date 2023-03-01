316d431e91c47aad2c33964d7f69a35cecc7529f
kreta-kat <85033106+kreta-kat@users.noreply.github.com>
2023-03-01 21:13:48 +0530

    Create Avoid Traps.cpp

---
+++ Miscellaneous/Avoid Traps.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+int avoidTraps(vector<int>& obstacles, int n) 
+{
+    // Write your code here.
+    int no= INT_MAX;
+    sort(obstacles.begin(), obstacles.end());
+    for(int i=obstacles[0]; i<n; i++)
+    {
+         if(obstacles[i] != i)
+         {
+             
+         }
+
+         
+    }
+    return no;
+
+}
