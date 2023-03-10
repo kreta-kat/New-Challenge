2c698af15144ddaa6861f2d1dc0fc0f734f0cfe0
kreta-kat <85033106+kreta-kat@users.noreply.github.com>
2023-03-10 21:07:14 +0530

    Create Print Pattern.cpp

---
+++ Miscellaneous/Print Pattern.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+void printPatt(int n) {
+  // Write your code here.
+  for(int i=0;i<n;i++)
+  {
+    for(int j=i;j<n;j++)
+    {
+      if(i%2 ==0)
+      cout<<"1";
+      else
+      cout<<"0";
+    }
+    cout<<endl;
+  }
+}
