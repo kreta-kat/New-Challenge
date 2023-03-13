2f12373455d939a351b14c62990a29f70afd84db
kreta-kat <85033106+kreta-kat@users.noreply.github.com>
2023-03-13 19:52:58 +0530

    Update 2.cpp

--- Miscellaneous/2.cpp
+++ Miscellaneous/2.cpp
@@ -1,1 +1,3 @@
 
+weddlvmvv
+Amit orders a gift from 4 different websites for his friend's birthday. The probability of the sites delivering on time is 0.9,0.8,0.7 and 0.6 respectively. What is the probability that the friend would get the gift on time? (answer up to 4 decimal places)
